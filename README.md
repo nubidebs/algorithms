9ac38019fee515f4ad1a88fd990917867518cdbe
Deb <63707881+nubidebs@users.noreply.github.com>
2022-09-16 21:09:46 +0300

    RGB to hex conversion

--- README.md
+++ README.md
@@ -215,3 +215,19 @@ Write a function `dirReduc` which will take an array of strings and returns an a
 Notes
 
 Not all paths can be made simpler. The path `["NORTH", "WEST", "SOUTH", "EAST"]` is not reducible. "NORTH" and "WEST", "WEST" and "SOUTH", "SOUTH" and "EAST" are not directly opposite of each other and can't become such. Hence the result path is itself : `["NORTH", "WEST", "SOUTH", "EAST"]`.
+
+
+## [RGB To Hex Conversion](https://www.codewars.com/kata/513e08acc600c94f01000001/javascript) (5kyu)
+
+The rgb function is incomplete. Complete it so that passing in `RGB` decimal values will result in a `hexadecimal representation` being returned. Valid decimal values for `RGB` are `0 - 255`. Any values that fall out of that range must be rounded to the closest valid value.
+
+Note: Your answer should always be `6 characters long`, the shorthand with 3 will not work here.
+
+The following are examples of expected output values:
+
+```
+rgb(255, 255, 255) // returns FFFFFF
+rgb(255, 255, 300) // returns FFFFFF
+rgb(0,0,0) // returns 000000
+rgb(148, 0, 211) // returns 9400D3
+```
---
+++ RGBtoHexConversion.js
@@ -0,0 +1,40 @@
+/* 5kyu
+The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned.
+Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
+
+The following are examples of expected output values:
+
+rgb(255, 255, 255) // returns FFFFFF
+rgb(255, 255, 300) // returns FFFFFF
+rgb(0,0,0) // returns 000000
+rgb(148, 0, 211) // returns 9400D3
+
+
+*/
+
+// First Solution
+
+function rgb(r, g, b){
+ 
+    const hex= [0,1,2,3,4,5,6,7,8,9,"A", "B", "C", "D", "E", "F"]
+  
+  const result = [r,g,b].map((color, index)=>{
+    if (color <= 0) return "00"
+    if (color >= 255) return "FF"
+    
+   
+    let firstDigit = +((color/16).toString().split(".")[0])
+    let secondDigit=+("0." + (color/16).toString().split(".")[1])*16
+   
+    const hexDigit1 = Object.is(firstDigit, NaN) ? 0 : firstDigit <= 16 ? hex[firstDigit] : "FF"
+    const hexDigit2 = Object.is(secondDigit, NaN) ? 0 : secondDigit <= 16 ? hex[secondDigit] : "FF"
+   
+    return `${hexDigit1}${hexDigit2}`
+  })
+  
+    return result.join("")
+  }
+
+  //Refactored Solution
+ 
