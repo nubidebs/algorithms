883a6023059b33716c0d922a18d02d84cdf21bbe
nubidebs <debora.crescenzo@gmail.com>
2024-02-21 08:10:58 +0000

    Draft

--- linked-list.js
+++ linked-list.js
@@ -1,3 +1,15 @@
+/*
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ *
+ * To print all the data inside we just have to traverse through all the nodes. 
+ * To do this, we use a while loop, as long as the node (identified by head) is not null,
+ * we keep printing it’s data and move to the next node.
+ */
+
 function printLinkedList(head) {
     while (head) {
         console.log(head.data);
